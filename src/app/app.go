8a3f09912001e9dd5735cd3d4f578848f2424941
Christiano Rizental <rizental@gmail.com>
2019-08-13 17:42:22 -0300

    desafio CI

--- src/app/app.go
+++ src/app/app.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func Sum(a int, b int) int {
-    return a + b
+    return a + b;
 }
 
 func main()
     res := Sum(5, 5)
-    fmt.Println("5 + 5 =", res)
+    fmt.Println("5 + 5 = ", res)
 }
